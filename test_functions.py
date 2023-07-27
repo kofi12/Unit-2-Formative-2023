cda300ec60e4248f0f09c2ea9f2e49e26eb454eb
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2023-07-27 11:57:02 -0400

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,21 +1,17 @@
-
 import pytest
 import operations
 
 def test_even_odd():
     assert opertions.check_even_odd(34) == "even"
     assert operations.check_even_odd(23) == "odd"
-    pass
     
 
 def test_factorial():
     assert operations.factorial(10) == 3628800
     assert operations.factorial(7) == 5040
-    pass
 
 
 def test_convert_temperature():
     assert operations.convert_temperature(25, C) == 77
     assert operations.convert_temperature(96, F) == 35.55
-    pass
 
