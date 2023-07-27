d240dbf7d5df2e544bb1a24e30004242f6fb9a82
kofi12 <kofi12@my.yorku.ca>
2023-07-27 11:38:07 -0400

    finished test cases

--- test_functions.py
+++ test_functions.py
@@ -3,13 +3,19 @@ import pytest
 import operations
 
 def test_even_odd():
+    assert check_even_odd(34) == "even"
+    assert check_even_odd(23) == "odd"
     pass
     
 
 def test_factorial():
+    assert factorial(10) == 3628800
+    assert factorial(7) == 5040
     pass
 
 
 def test_convert_temperature():
+    assert convert_temperature(25, C) == 77
+    assert convert_temperature(96, F) == 35.55
     pass
 
