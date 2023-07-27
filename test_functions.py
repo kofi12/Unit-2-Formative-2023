4c9f20821f847bef58351848fb3d7a63ce4183ae
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2023-07-27 11:47:05 -0400

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,21 +1,21 @@
-from numpy import true_divide
+
 import pytest
 import operations
 
 def test_even_odd():
-    assert check_even_odd(34) == "even"
-    assert check_even_odd(23) == "odd"
+    assert opertions.check_even_odd(34) == "even"
+    assert operations.check_even_odd(23) == "odd"
     pass
     
 
 def test_factorial():
-    assert factorial(10) == 3628800
-    assert factorial(7) == 5040
+    assert operations.factorial(10) == 3628800
+    assert operations.factorial(7) == 5040
     pass
 
 
 def test_convert_temperature():
-    assert convert_temperature(25, C) == 77
-    assert convert_temperature(96, F) == 35.55
+    assert operations.convert_temperature(25, C) == 77
+    assert operations.convert_temperature(96, F) == 35.55
     pass
 
