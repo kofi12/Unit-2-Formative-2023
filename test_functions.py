6185671a8cd5962305578dc3b89dc2c2aecb9472
kofi12 <kofi12@my.yorku.ca>
2023-07-27 11:31:00 -0400

    added test cases

---
+++ test_functions.py
@@ -0,0 +1,15 @@
+from numpy import true_divide
+import pytest
+import operations
+
+def test_even_odd():
+    pass
+    
+
+def test_factorial():
+    pass
+
+
+def test_convert_temperature():
+    pass
+
